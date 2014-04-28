10c1c1182df60bb4b28106286ef04670ec522657
jphager2 <jphager2@gmail.com>
2014-04-28 11:43:42 +0200

    added partials for navigation to public

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,8 +1,6 @@
 module PagesHelper
   def load_github_activity 
-    @events = JSON(
-      open('https://api.github.com/users/jphager2/events/public').read
-    )[0..4] 
+    @events = JSON(open('https://api.github.com/users/jphager2/events/public').read)[0..4] 
   end
 end
 
