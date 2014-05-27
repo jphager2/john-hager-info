084f9c3768129f20e86fdb80d8c98a48b468b52d
jphager2 <jphager2@gmail.com>
2014-05-27 10:52:16 +0200

    cleaned up pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,28 +1,27 @@
 class PagesController < ApplicationController
-  def index
+  def initialize
+    super
     load_github_activity
+  end
+
+  def index
   end
 
   def go
-    load_github_activity
   end
 
   def contact
-    load_github_activity
   end
 
   def cv
-    load_github_activity
   end
 
   def portfolio
     @projects = Project.all
-    load_github_activity
   end
 
   def admin
     redirect_to root_path unless signed_in?
     @projects = Project.all
-    load_github_activity
   end
 end
