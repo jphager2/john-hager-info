50580d34b0c079cb6848c07f391bc4bbd6fd29e1
jphager2 <jphager2@gmail.com>
2015-01-28 10:08:06 +0100

    heroku-wkhtmlto-pdf seting config path

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,5 +1,5 @@
-WickedPdf.config = {
+#WickedPdf.config = {
   #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
   #:layout => "pdf.html",
-  :exe_path => `which wkhtmltopdf-linux-amd64`.chomp
-}
+  #:exe_path => `which wkhtmltopdf-linux-amd64`.chomp
+#}
