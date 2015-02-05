983005f2b05cfcbfffe5173ecbe848fcb7d7334e
jphager2 <jphager2@gmail.com>
2015-02-05 14:02:17 +0100

    authenticates user for admin section

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,10 @@
 class AdminController < ApplicationController
   layout 'admin'
   skip_before_action :load_github_events
+  before_action :authenticate_user
+
+  private
+  def authenticate_user
+    redirect_to root_path unless signed_in?
+  end
 end
