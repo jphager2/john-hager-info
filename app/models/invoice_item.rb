e1eb5748c685388c67eb3c70cfe82cb9a47df2ec
jphager2 <jphager2@gmail.com>
2015-02-05 13:13:56 +0100

    validations for invoice items

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,7 +1,21 @@
 class InvoiceItem < ActiveRecord::Base
   belongs_to :invoice
 
+  before_validation :ensure_currency
+  before_validation :ensure_price
+
+  validates :title, presence: true
+
   def human_name
     self.class.to_s.sub(/Item/, '').pluralize
+  end
+
+  private
+  def ensure_currency
+    self.currency ||= Invoice::Currencies.first
+  end
+
+  def ensure_price
+    self.price ||= 0
   end
 end
