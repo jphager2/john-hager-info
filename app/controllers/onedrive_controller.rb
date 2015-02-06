33499ae71bdcd67af726e6beaa16cb3aba321189
jphager2 <jphager2@gmail.com>
2015-02-06 15:54:25 +0100

    first attempt to curl to od

--- app/controllers/onedrive_controller.rb
+++ app/controllers/onedrive_controller.rb
@@ -3,5 +3,20 @@ class OnedriveController < AdminController
   end
 
   def create
+    if params[:code].present?
+      {
+        "CLIENT_ID" => ENV["ONEDRIVE_CLIENT_ID"],
+        "REDIRECT_URI" => "http://www.john-hager.info/onedrive",
+        "CLIENT_SECRET" => ENV["ONEDRIVE_CLIENT_SECRET"],
+        "AUTHORIZATION_CODE" => params[:code],
+      }
+      curl('https://login.live.com/oauth20_token.srf',parameters,:post)
+    end
+  end
+
+  private 
+  def curl(path, parameters, method)
+    path = "#{path}?#{CGI.escape(parameters)}"
+    `curl #{path} -X #{method.to_s.upcase} -H "Content-Type: application/x-www-form-urlencoded"`
   end
 end
