ae9221b6b80450ad19c1ba5421857e0f22bbeb04
jphager2 <jphager2@gmail.com>
2015-02-09 20:43:12 +0100

    setting auth in onedrive

--- app/controllers/onedrive_controller.rb
+++ app/controllers/onedrive_controller.rb
@@ -4,16 +4,20 @@ class OnedriveController < AdminController
   skip_before_action :set_client
 
   def login
-    auth = Skydrive::Oauth::Client.new(ENV["ONEDRIVE_CLIENT_ID"], ENV["ONEDRIVE_CLIENT_SECRET"], "http://www.john-hager.info/onedrive", "wl.skydrive_update,wl.offline_access")
-    redirect_to auth.authorize_url 
+    redirect_to @auth.authorize_url 
   end
 
   def create
     if params[:code].present?
-      @access_token = @client.get_access_token(params[:code])
+      @access_token = @auth.get_access_token(params[:code])
       session[:access_token] = @access_token
     elsif params[:refresh_token].present?
       30.times { puts "=*" * 30 } 
     end
+  end
+
+  private
+  def set_auth
+    @auth = Skydrive::Oauth::Client.new(ENV["ONEDRIVE_CLIENT_ID"], ENV["ONEDRIVE_CLIENT_SECRET"], "http://www.john-hager.info/onedrive", "wl.skydrive_update,wl.offline_access")
   end
 end
