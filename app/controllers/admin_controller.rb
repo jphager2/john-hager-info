2e3a03533c8f019801d8116d3c6d5932f16aef17
jphager2 <jphager2@gmail.com>
2015-02-09 19:04:06 +0100

    loading client from access token if available

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,10 +1,21 @@
 class AdminController < ApplicationController
   layout 'admin'
   skip_before_action :load_github_events
+
   before_action :authenticate_user
+  before_action :set_client
 
   private
   def authenticate_user
     redirect_to root_path unless signed_in?
   end
+
+  def set_client
+    if session[:access_token] 
+      @client = Skydrive::Oauth::Client(session[:access_token])
+    else
+      @client = Skydrive::Oauth::Client.new(ENV["ONEDRIVE_CLIENT_ID"], ENV["ONEDRIVE_CLIENT_SECRET"], "http://www.john-hager.info/onedrive", "wl.skydrive_update,wl.offline_access")
+    end
+  end
+
 end
--- app/controllers/onedrive_controller.rb
+++ app/controllers/onedrive_controller.rb
@@ -1,7 +1,6 @@
 require 'faraday'
 class OnedriveController < AdminController
   skip_before_action :authenticate_user
-  before_action :set_client
 
   def login
     redirect_to @client.authorize_url 
@@ -10,15 +9,7 @@ class OnedriveController < AdminController
   def create
     if params[:code].present?
       @access_token = @client.get_access_token(params[:code])
-      puts '+=' * 50
-      puts @access_token
-      puts @client.to_s
-      puts '+=' * 50
+      session[:access_token] = @access_token
     end
-  end
-
-  private 
-  def set_client
-    @client = Skydrive::Oauth::Client.new(ENV["ONEDRIVE_CLIENT_ID"], ENV["ONEDRIVE_CLIENT_SECRET"], "http://www.john-hager.info/onedrive", "wl.skydrive_update,wl.offline_access")
   end
 end
