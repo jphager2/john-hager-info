537ff470d8726e0c4b32fc977d0e0e6e07d3066e
jphager2 <jphager2@gmail.com>
2015-04-07 16:44:18 +0200

    removed visualization

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -44,9 +44,6 @@ class ClientsController < AdminController
   def update
     respond_to do |format|
       if @client.update(client_params)
-        @client_params = client_params
-        pp @client.attributes
-        pp @client_params
         format.html { redirect_to @client, notice: 'Client was successfully updated.' }
         format.json { render :show, status: :ok, location: @client }
       else
