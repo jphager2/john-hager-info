b739472e2b26f78a2fce75e2bd64860e5b533869
jphager2 <jphager2@gmail.com>
2018-04-07 01:06:59 +0200

    Fix before action name

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,25 +1,13 @@
 class AdminController < ApplicationController
   layout 'admin'
-  skip_before_action :load_github_events
 
-  around_action :log_exceptions
+  skip_before_action :load_github_activity
+
   before_action :authenticate_user
 
-  private
+  protected
+
   def authenticate_user
     redirect_to root_path unless signed_in?
-  end
-
-  def log_exceptions
-    yield
-  rescue Object => e
-    puts "\n" * 3
-    puts 'Error Caught in Admin Controller'
-    puts e, e.message
-    puts "\n" * 3
-    raise
-  rescue Skydrive::Error => e
-    flash[:notice] = "Token Expired" if e.code == "http_error_401" 
-    redirect_to onedrive_login_path
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,8 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
   include SessionsHelper
   include PagesHelper
+
+  protect_from_forgery with: :exception
 
   before_action :load_github_activity
 end
